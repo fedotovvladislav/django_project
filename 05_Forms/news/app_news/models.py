59b5b4b0abf7768ae6f13089752d7740ae9b669e
KorotkovMisha <mikhailkorotkov08200@gmail.com>
2022-10-10 09:39:07 +0300

    correct models

--- 05_Forms/news/app_news/models.py
+++ 05_Forms/news/app_news/models.py
@@ -3,14 +3,22 @@ from django.core.validators import MinLengthValidator
 
 
 class NewsModel(models.Model):
+    STATUS_CHOICES = [
+        ('a', 'Активна'),
+        ('n', 'Не активна')
+    ]
+
     name = models.CharField(validators=[MinLengthValidator(10)], max_length=200, verbose_name='Заголовок')
     description = models.TextField(validators=[MinLengthValidator(200)], max_length=10000, verbose_name='Содержание')
     date_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     date_edit = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
-    active = models.BooleanField(default=True, verbose_name='Активна')
+    active = models.BooleanField(max_length=1, choices=STATUS_CHOICES, default='a', verbose_name='Статус')
 
     class Meta:
         ordering = ['-date_create']
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class CommentsModel(models.Model):
@@ -18,3 +26,6 @@ class CommentsModel(models.Model):
     comment = models.TextField(validators=[MinLengthValidator(10)], max_length=3000, verbose_name='Текст комментария')
     news = models.ForeignKey('NewsModel', on_delete=models.CASCADE, null=False,
                              related_name='comments', verbose_name='Новость')
+
+    def __str__(self):
+        return f'{self.user_name}: {self.comment[:15]}...'
